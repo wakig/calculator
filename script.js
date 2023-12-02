a6bc0dfb3f568700972c70b66c3188e5dbb32508
Waki G <37522888+wakig@users.noreply.github.com>
2023-12-02 22:10:09 +0800

    Added keyboard events. Still need to improve decimal logic

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let first_ok = false;
 let second_ok = false;
 let decimal_ok = false;
 let operator_ok = false;
+let reset_display = false;
+const ROUND = 100000000;
+const number_keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const operator_keys = ['+', '-', 'x', '*', '/'];
 
 let first_number = 0;
 let second_number = 0;
@@ -33,21 +37,29 @@ plusminus.addEventListener('click', invert_sign);
 const percent = document.querySelector('.percent');
 percent.addEventListener('click', press_percent);
 
+const backspace = document.querySelector('.backspace');
+backspace.addEventListener('click', press_backspace);
+
 const screen = document.querySelector('.screen');
+
+document.addEventListener('keydown', key_press);
 
 function press_operator(e) {
     if (first_ok && !operator_ok && !second_ok) {
         first_number = parseFloat(screen.textContent);
-        operator_text = e.currentTarget.textContent;
+        screen.textContent = '';
+        if (e instanceof KeyboardEvent) operator_text = e.key;
+        else operator_text = e.currentTarget.textContent;
         operator_ok = true;
     }
     else if (first_ok && operator_ok && second_ok) {
         press_equals();
-        operator_text = e.currentTarget.textContent;
+        screen.textContent = first_number.toString();
+        if (e instanceof KeyboardEvent) operator_text = e.key;
+        else operator_text = e.currentTarget.textContent;
         operator_ok = true;
+        reset_display = true;
     }
-    // console.log(first_number, second_number);
-    screen.textContent = '';
 }
 
 function press_equals() {
@@ -55,16 +67,17 @@ function press_equals() {
         second_number = parseFloat(screen.textContent);
         if (operator_text === '+') add(first_number, second_number);
         else if (operator_text === '-') subtract(first_number, second_number);
-        else if (operator_text === 'x') multiply(first_number, second_number);
+        else if (operator_text === 'x' || operator_text === '*') multiply(first_number, second_number);
         else if (operator_text === '/') divide(first_number, second_number);
         operator_ok = false;
-        console.log(first_number, second_number);
+        // console.log(first_number, second_number);
     }
 }
 
 // addition
 function add(a, b) {
     first_number = a + b;
+    first_number = Math.round(first_number * ROUND) / ROUND;
     screen.textContent = first_number.toString();
     second_ok = false;
 }
@@ -72,6 +85,7 @@ function add(a, b) {
 // subtraction
 function subtract(a, b) {
     first_number = a - b;
+    first_number = Math.round(first_number * ROUND) / ROUND;
     screen.textContent = first_number.toString();
     second_ok = false;
 }
@@ -79,6 +93,7 @@ function subtract(a, b) {
 // multiplication
 function multiply(a, b) {
     first_number = a * b;
+    first_number = Math.round(first_number * ROUND) / ROUND;
     screen.textContent = first_number.toString();
     second_ok = false;
 }
@@ -92,6 +107,7 @@ function divide(a, b) {
         }
         else {
             first_number = a / b;
+            first_number = Math.round(first_number * ROUND) / ROUND;
             screen.textContent = first_number.toString();
             second_ok = false;
         }
@@ -100,7 +116,14 @@ function divide(a, b) {
 
 // press number
 function press_number(e) {
-    screen.textContent += e.currentTarget.textContent;
+    if (reset_display) {
+        screen.textContent = '';
+        reset_display = false;
+    }
+
+    if (e instanceof KeyboardEvent) screen.textContent += e.key;
+    else screen.textContent += e.currentTarget.textContent;
+
     if (!operator_ok) first_ok = true;
     else second_ok = true;
 }
@@ -109,7 +132,8 @@ function press_number(e) {
 function press_decimal(e) {
     if (!decimal_ok) {
         decimal_ok = true;
-        screen.textContent += e.currentTarget.textContent;
+        if (e instanceof KeyboardEvent) screen.textContent += e.key;
+        else screen.textContent += e.currentTarget.textContent;
     }
     if (!operator_ok) first_ok = true;
     else second_ok = true;
@@ -134,4 +158,32 @@ function invert_sign() {
 function press_percent() {
     let temp = parseFloat(screen.textContent) / 100.0;
     screen.textContent = temp.toString();
+}
+
+function press_backspace() {
+    if (screen.textContent) screen.textContent = screen.textContent.substring(0, screen.textContent.length-1);
+}
+
+function key_press(e) {
+    let key_name = e.key;
+    console.log(key_name);
+
+    if (number_keys.includes(key_name)) {
+        press_number(e);
+    }
+    else if (operator_keys.includes(key_name)) {
+        press_operator(e);
+    }
+    else if (key_name === '.') {
+        press_decimal(e);
+    }
+    else if (key_name === '=') {
+        press_equals();
+    }
+    else if (key_name === 'Escape') {
+        reset();
+    }
+    else if (key_name === 'Backspace') {
+        press_backspace();
+    }
 }
